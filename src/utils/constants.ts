f3348cb00a577798dff38dec23be5fc23a266ff6
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-10-03 15:53:59 +0300

    fix: route naming

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,7 @@ export const ABOUT_ROUTE = '/about-us';
 export const VOCAB_ROUTE = '/vocabulary';
 export const VOCAB_ACTIVE_ROUTE = '/vocabulary/active';
 export const VOCAB_INACTIVE_ROUTE = '/vocabulary/inactive';
-export const VOCAB_LEARNED_ROUTE = '/vocabulary/learned';
+export const VOCAB_LEARNED_ROUTE = '/vocabulary/mastered';
 
 export const VOCAB_DANGER_ROUTE = '/vocabulary/danger';
 export const VOCAB_POTENTIAL_ROUTE = '/vocabulary/potential';
