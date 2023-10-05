3f1c2912ec5553e7f2d5bc82fb6e9691d6c572b2
azozulya <bbbom@mail.ru>
2023-10-05 15:58:17 +0300

    feat: add translation to words list and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import styles from './Header.module.scss';
+import { useTranslation } from 'react-i18next';
 
 import { Button } from '@ui/Button';
-import CustomLink from '../CustomLink';
+import { ButtonIcon } from '@ui/ButtonIcon';
 import Input from '@ui/Input';
+import CustomLink from '../CustomLink';
 import ThemeSwitcher from '../ThemeSwitcher';
 import SelectLang from '../SelectLang';
 
@@ -12,10 +13,12 @@ import iconTags from '@assets/icons/icon_tags.svg';
 import SvgIconLogo from '@assets/icons/logo.svg?react';
 import SvgIconNotification from '@assets/icons/icon_notification.svg?react';
 import SvgIconUserCabinet from '@assets/icons/icon_user_cabinet.svg?react';
-import { ButtonIcon } from '../ui/ButtonIcon';
+
+import styles from './Header.module.scss';
 
 const Header = () => {
 	const [searchText, changeSearchText] = useState('');
+	const { t } = useTranslation('header');
 
 	return (
 		<header>
@@ -36,13 +39,13 @@ const Header = () => {
 					className={styles.logoText}
 					onClick={() => console.log('click')}
 				>
-					Linguista <span>Control & Repeat</span>
+					{t('projectName')} <span>{t('projectSlogan')}</span>
 				</CustomLink>
 				<div className={styles.searching}>
 					<div className={styles.inputBlock}>
 						<Input
 							type="text"
-							placeholder="Найти слово или фразу.."
+							placeholder={t('searchPlaceholder')}
 							className={styles.searchInput}
 							onChange={(evt) => changeSearchText(evt.target.value)}
 							value={searchText}
@@ -61,7 +64,7 @@ const Header = () => {
 
 				{/* TODO вынести в отдельный кабинет с модальным окном */}
 				<Button className={styles.button} onClick={() => console.log('click')}>
-					+&nbsp;Добавить
+					{t('add')}
 				</Button>
 
 				<div className={styles.rightPanel}>
--- src/components/WordsList/WordsList.tsx
+++ src/components/WordsList/WordsList.tsx
@@ -1,10 +1,11 @@
+import { useTranslation } from 'react-i18next';
+import { Button } from '@ui/Button';
 import { IWordCard } from '@/shared/interfaces/IWordCard';
-import styles from './WordsList.module.scss';
 import { WordCard } from './elements/WordCard';
 import image from '@assets/mock-for-card.png';
 import SvgIconHistory from '@assets/icons/icon_history.svg?react';
 import SvgIconView from '@assets/icons/icon_view.svg?react';
-import { Button } from '../ui/Button';
+import styles from './WordsList.module.scss';
 
 const data: Array<IWordCard> = [
 	{
@@ -51,6 +52,8 @@ const data: Array<IWordCard> = [
 ];
 
 export const WordsList = () => {
+	const { t } = useTranslation('vocabulary');
+
 	const wordsList = data.map((wordItem, idx) => {
 		return <WordCard item={wordItem} key={idx} />;
 	});
@@ -59,14 +62,14 @@ export const WordsList = () => {
 		<section>
 			<div className={styles.header}>
 				<div className={styles.headerStats}>
-					<span className={styles.subTitle}>146 слов и&nbsp;фраз</span>
+					<span className={styles.subTitle}>{t('totalWords', { count: 142 })}</span>
 				</div>
 				<div className={styles.headerActions}>
 					<Button className={styles.btn} size="tall">
-						Добавить в&nbsp;словарь
+						{t('addToVocabulary')}
 					</Button>
 					<Button className={styles.btn} theme="transparent" size="tall">
-						Добавить в&nbsp;коллекцию
+						{t('addToCollection')}
 					</Button>
 				</div>
 				<div className={styles.headerLinks}>
