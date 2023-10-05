1f80eea7b14e0e7e5642c00db24860c1ca6ae383
azozulya <bbbom@mail.ru>
2023-10-05 15:53:31 +0300

    feat: add languages list to constant

--- i18n.ts
+++ i18n.ts
@@ -1,8 +1,15 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+import { LANGUAGES } from '@/utils/constants';
 
 export default i18n
 	.use(Backend)
 	.use(initReactI18next)
-	.init({ debug: true, lng: 'ru', interpolation: { escapeValue: false } });
+	.init({
+		debug: true,
+		lng: LANGUAGES[0],
+		fallbackLng: LANGUAGES,
+		ns: [], // files to load  ns: ['app', 'common'],
+		interpolation: { escapeValue: false },
+	});
