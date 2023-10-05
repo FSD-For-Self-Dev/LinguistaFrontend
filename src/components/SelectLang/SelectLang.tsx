5ed9e2e08297e2103c055823c55b33d5afaf56dc
azozulya <bbbom@mail.ru>
2023-10-05 15:59:59 +0300

    feat: change language functionality

--- src/components/SelectLang/SelectLang.tsx
+++ src/components/SelectLang/SelectLang.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react';
-import styles from './SelectLang.module.scss';
+import { ChangeEvent, useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ButtonIcon } from '@ui/ButtonIcon';
+import { LANGUAGES } from '@/utils/constants';
+
 import SvgIconLang from '@assets/icons/icon_globus.svg?react';
-import { ButtonIcon } from '../ui/ButtonIcon';
+import styles from './SelectLang.module.scss';
 
 const SelectLang = () => {
-	const [language, changeLanguage] = useState('RU');
+	const { i18n } = useTranslation();
+	const [language, setLanguage] = useState(i18n.language);
+
+	const changeLangHandler = useCallback(
+		(event: ChangeEvent<HTMLSelectElement>) => {
+			const newLang = event.target.value;
+
+			if (newLang === language) return;
+
+			setLanguage(newLang);
+			i18n.changeLanguage(newLang);
+		},
+		[i18n, language]
+	);
 
 	return (
 		<div className={styles.lang}>
@@ -13,11 +29,14 @@ const SelectLang = () => {
 					<SvgIconLang />
 					<select
 						className={styles.selectLang}
-						onChange={(evt) => changeLanguage(evt.target.value)}
+						onChange={changeLangHandler}
 						defaultValue={language}
 					>
-						<option value="RU">ru</option>
-						<option value="ENG">eng</option>
+						{LANGUAGES.map((lang, idx) => (
+							<option value={lang} key={`lang${idx}`} selected={lang === language}>
+								{lang}
+							</option>
+						))}
 					</select>
 				</>
 			</ButtonIcon>
