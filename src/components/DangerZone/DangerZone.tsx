ef9b3e9c2337580e176f1f19ab3896f0a2971804
azozulya <bbbom@mail.ru>
2023-10-05 15:56:06 +0300

    feat: add translation to danger zone

--- src/components/DangerZone/DangerZone.tsx
+++ src/components/DangerZone/DangerZone.tsx
@@ -1,28 +1,29 @@
 import { Link } from 'react-router-dom';
-
-import DangerZoneWord from '@components/DangerZone/DangerZoneWords';
-import { Carousel } from '@ui/Carousel/Carousel';
+import { useTranslation } from 'react-i18next';
 import { Button } from '@ui/Button';
+import { Carousel } from '@ui/Carousel/Carousel';
+import DangerZoneWord from '@components/DangerZone/DangerZoneWords';
+
 import triangle from '@assets/icons/warning-triangle.svg';
 import styles from './DangerZone.module.scss';
 
 function DangerZone() {
+	const { t } = useTranslation('danger-zone');
+
 	return (
 		<div className={styles.section}>
 			<div className={styles.container}>
 				<div className={styles.title_left}>
 					<img src={triangle} alt="warning triangle" />
-					<p className={styles.itemRed}>Зона опасности</p>
-					<p className={styles.itemGrey}>
-						Эти слова вот-вот станут неактивными
-					</p>
+					<p className={styles.itemRed}>{t('title')}</p>
+					<p className={styles.itemGrey}>{t('subTitle')}</p>
 				</div>
 				<div className={styles.title_right}>
-					<Button className={styles.button}>Спасти!</Button>
+					<Button className={styles.button}>{t('saveAll')}</Button>
 
 					<Link to="/dangerZoneCards" className={styles.linkButton}>
 						{/* TODO счетчик кол-ва карточек (вместо цифры 15) */}
-						Смотреть все (15)
+						{t('seeAll')} (15)
 					</Link>
 				</div>
 			</div>
