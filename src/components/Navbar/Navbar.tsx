ede5e695b5eaf4e91166e113b3abc53e151f996c
azozulya <bbbom@mail.ru>
2023-10-07 22:21:22 +0300

    fix: navbar translation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,24 @@
 import cx from 'classnames';
 import CustomLink from '@components/CustomLink';
+import { useTranslation } from 'react-i18next';
 
+import SvgIconInfo from '@assets/icons/icon_info.svg?react';
 import SvgIconHome from '@assets/icons/icon_home.svg?react';
+import SvgIconFavorite from '@assets/icons/icon_favorite.svg?react';
+import SvgIconExercises from '@assets/icons/icon_exercises.svg?react';
 import SvgIconLanguages from '@assets/icons/icon_languages.svg?react';
 import SvgIconDictionary from '@assets/icons/icon_dictionary.svg?react';
 import SvgIconCollections from '@assets/icons/icon_collections.svg?react';
-import SvgIconFavorite from '@assets/icons/icon_favorite.svg?react';
-import SvgIconExercises from '@assets/icons/icon_exercises.svg?react';
-import SvgIconInfo from '@assets/icons/icon_info.svg?react';
 import styles from './Navbar.module.scss';
+import { useCallback } from 'react';
 
 function Navbar() {
-	const scrollToUp = (id: string) => {
+	const { t } = useTranslation('navbar');
+
+	const scrollToUp = useCallback((id: string) => {
 		const element = document.getElementById(id);
 		element && element.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-	};
+	}, []);
 
 	return (
 		<aside className={styles.container}>
@@ -28,7 +32,7 @@ function Navbar() {
 							onClick={() => console.log('click')}
 						>
 							<SvgIconHome className={styles.icon} />
-							<h2 className={styles.title}>Главная</h2>
+							<h2 className={styles.title}>{t('home')}</h2>
 						</CustomLink>
 					</li>
 					<li>
@@ -39,7 +43,7 @@ function Navbar() {
 							onClick={() => console.log('click')}
 						>
 							<SvgIconLanguages className={styles.icon} />
-							<h2 className={styles.title}>Языки</h2>
+							<h2 className={styles.title}>{t('languages')}</h2>
 						</CustomLink>
 					</li>
 					<li>
@@ -50,7 +54,7 @@ function Navbar() {
 							onClick={() => console.log('click')}
 						>
 							<SvgIconDictionary className={styles.icon} />
-							<h2 className={styles.title}>Словарь</h2>
+							<h2 className={styles.title}>{t('vocabulary')}</h2>
 						</CustomLink>
 					</li>
 					<li>
@@ -61,7 +65,7 @@ function Navbar() {
 							onClick={() => console.log('click')}
 						>
 							<SvgIconCollections className={styles.icon} />
-							<h2 className={styles.title}>Коллекции</h2>
+							<h2 className={styles.title}>{t('collections')}</h2>
 						</CustomLink>
 					</li>
 					<li>
@@ -72,7 +76,7 @@ function Navbar() {
 							onClick={() => console.log('click')}
 						>
 							<SvgIconFavorite className={styles.icon} />
-							<h2 className={styles.title}>Избранное</h2>
+							<h2 className={styles.title}>{t('favorite')}</h2>
 						</CustomLink>
 					</li>
 					<hr className={styles.border}></hr>
@@ -84,7 +88,7 @@ function Navbar() {
 							onClick={() => console.log('click')}
 						>
 							<SvgIconExercises className={styles.icon} />
-							<h2 className={styles.title}>Упражнения</h2>
+							<h2 className={styles.title}>{t('exercises')}</h2>
 						</CustomLink>
 					</li>
 					<hr className={styles.border}></hr>
@@ -96,7 +100,7 @@ function Navbar() {
 							onClick={() => console.log('click')}
 						>
 							<SvgIconInfo className={`${styles.icon} ${styles.icon_circle}`} />
-							<h2 className={styles.title}>О проекте</h2>
+							<h2 className={styles.title}>{t('about')}</h2>
 						</CustomLink>
 					</li>
 				</ul>
