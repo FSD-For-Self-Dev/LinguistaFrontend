8b2cd672f39b5796e580f5d31fdc78b184e8b36d
Maria Gromyko <mazvereva91@mail.ru>
2023-10-07 21:32:45 +0300

    feat:block-layout

---
+++ src/modules/Associations/Associations.tsx
@@ -0,0 +1,28 @@
+import CustomLink from '@/components/ui/CustomLink';
+import { SectionTitle } from '@/components/ui/SectionTitle';
+import SvgIconAssociations from '@assets/icons/icon_associations.svg?react';
+import styles from './Associations.module.scss';
+import AssociationsList from './AssociationsList';
+import { dataAssociation } from './mocks';
+
+/* eslint-disable prettier/prettier */
+const Associations = () => {
+    const count = dataAssociation.length;
+    return (
+        <section className={styles.content}>
+            <div className={styles.header}>
+                <SectionTitle>
+                    <SvgIconAssociations />
+                    Ассоциации
+                </SectionTitle>
+                <CustomLink href="#" target="_blank" apperance="button">
+                    Все ассоциации&nbsp;
+                    <span className={styles.count}>{count}</span>
+                </CustomLink>
+            </div>
+            <AssociationsList />
+        </section >
+    )
+};
+
+export default Associations;
