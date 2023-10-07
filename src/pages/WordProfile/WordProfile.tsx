9a3209cf735a27781d44905ceb175d527d7074dd
Maria Gromyko <mazvereva91@mail.ru>
2023-10-07 18:46:37 +0300

    fix:clearing-comments

--- src/pages/WordProfile/WordProfile.tsx
+++ src/pages/WordProfile/WordProfile.tsx
@@ -1,8 +1,5 @@
-// import { WordsList } from '@/components/WordsList';
 import WordCollection from '@/modules/WordCollection';
-
 import WordInfo from '@components/WordInfo';
-// import { Outlet } from 'react-router-dom';
 
 export default function WordProfile() {
 	return (
