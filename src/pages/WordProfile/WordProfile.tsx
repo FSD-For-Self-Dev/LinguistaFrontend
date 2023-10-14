6bbf91fe4c711d0f81dcc7e6259de8b867760f0b
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-10-14 19:21:07 +0300

    fix: add page flex

--- src/pages/WordProfile/WordProfile.tsx
+++ src/pages/WordProfile/WordProfile.tsx
@@ -1,11 +1,12 @@
 import WordCollection from '@/modules/WordCollection';
 import WordInfo from '@modules/WordMainInfo/WordInfo';
+import styles from './WordProfile.module.scss';
 
 export default function WordProfile() {
 	return (
-		<>
+		<div className={styles.container}>
 			<WordInfo />
 			<WordCollection />
-		</>
+		</div>
 	);
 }
