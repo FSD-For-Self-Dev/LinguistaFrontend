c5a1c52d1b4d77004d17b70cf0ea6b4ea0dc0293
Maria Gromyko <mazvereva91@mail.ru>
2023-10-14 23:27:37 +0400

    fix: change span

--- src/modules/WordCollection/CardCollection/CardCollection.tsx
+++ src/modules/WordCollection/CardCollection/CardCollection.tsx
@@ -9,7 +9,7 @@ const CardCollection = () => {
 			<div className={styles.card}>
 				<div className={styles.header}>
 					<h3>Базовые</h3>
-					<label className={styles.label}>49</label>
+					<span className={styles.label}>49</span>
 				</div>
 				<div className={styles.description}>
 					{items.map((item, id) => (
