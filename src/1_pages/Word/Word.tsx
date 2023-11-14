6e46e3a1679f669655c43bb912106ed137ee759e
azozulya <bbbom@mail.ru>
2023-11-14 12:31:14 +0200

    feat: add definitions to word page

--- src/1_pages/Word/Word.tsx
+++ src/1_pages/Word/Word.tsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { IWord } from '@entities/word/model/types';
+import { wordApi } from '@entities/word/api/wordApi';
 import WordInfo from '@widgets/WordMainInfo/WordInfo';
-import WordCollection from '@widgets/WordCollection';
-import Associations from '@widgets/Associations';
 import Synonyms from '@widgets/SynonymsAntonyms/Synonyms';
 import Antonyms from '@widgets/SynonymsAntonyms/Antonyms';
+import Definitions from '@widgets/Definitions';
+import Associations from '@widgets/Associations';
+import WordCollection from '@widgets/WordCollection';
 import styles from './Word.module.scss';
 
 const Word = () => {
+	const navigate = useNavigate();
+	const { slugWord: wordId } = useParams();
+	const [word, setWord] = useState<IWord | null>(null);
+
+	useEffect(() => {
+		if (!wordId) return navigate('/404');
+
+		wordApi
+			.getWordById(wordId)
+			.then((data: IWord) =>
+				setWord((prevState) => (prevState ? { ...prevState, ...data } : data))
+			);
+	}, [navigate, wordId]);
+
 	return (
 		<div className={styles.container}>
+			{word?.text}
 			<WordInfo />
 			<WordCollection />
 			<Associations />
+			{word?.definitions && <Definitions count={'10'} items={word?.definitions} />}
 			<Synonyms />
 			<Antonyms />
 		</div>
