2f91e9b798669b4444a47be05456d4c0e84a0a51
azozulya <bbbom@mail.ru>
2024-01-22 12:43:41 +0200

    refactor: DefinitionWidget on the word page

--- src/2_widgets/Definitions/DefinitionsWidget/DefinitionsWidget.tsx
+++ src/2_widgets/Definitions/DefinitionsWidget/DefinitionsWidget.tsx
@@ -1,8 +1,10 @@
 import { useTranslation } from 'react-i18next';
-import { DefinitionsList } from '../DefinitionsList';
-import { WidgetContainer, WidgetHeader, WidgetFooter, WidgetContent } from '@shared/ui/widget';
+import { DefinitionItem } from './DefinitionItem';
+import { ShadowBlockHeader } from '@entities/word/ui';
 import { ALL_DEFINITIONS, VOCAB_ROUTE } from '@shared/lib/routes';
+import { ShadowBlock, ShadowBlockContent, ShadowBlockFooter } from '@shared/ui';
 import type { TDefinition } from '@entities/word/model/types';
+import styles from './DefinitionsWidget.module.scss';
 
 type TProps = {
 	count: string;
@@ -15,12 +17,18 @@ export const DefinitionsWidget = ({ count, items, wordId }: TProps) => {
 	const link = `${VOCAB_ROUTE}/${wordId}${ALL_DEFINITIONS}`;
 
 	return (
-		<WidgetContainer>
-			<WidgetHeader title={t('definitions')} count={count} />
-			<WidgetContent>
-				<DefinitionsList items={items} />
-			</WidgetContent>
-			<WidgetFooter title={t('allDefinitions')} link={link} />
-		</WidgetContainer>
+		<ShadowBlock withPadding={false}>
+			<ShadowBlockHeader title={t('definitions')} count={count} />
+
+			<ShadowBlockContent>
+				<ul className={styles.list}>
+					{items.map((item, index) => (
+						<DefinitionItem key={item.id} item={item} index={index + 1} />
+					))}
+				</ul>
+			</ShadowBlockContent>
+
+			<ShadowBlockFooter title={t('allDefinitions')} link={link} />
+		</ShadowBlock>
 	);
 };
